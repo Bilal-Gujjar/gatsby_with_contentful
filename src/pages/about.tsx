48c5f4f8d6162f213e69170af2d360a1f02fd2ab
Bilal Gujjar <bilaltahir515@gmail.com>
2020-10-10 00:18:45 +0500

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ export default function about() {
     return (<Layout>
         <div>
             
-            <h1><u>Hello from the About page</u></h1>
+            <h1><u>Hello from the About page..</u></h1>
            
         </div> </Layout>
     )
